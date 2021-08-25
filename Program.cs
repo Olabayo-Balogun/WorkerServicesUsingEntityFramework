560234a7112bdaa770700142f153f8b557a4a5ff
OLABAYO BALOGUN <olabayobalogun@gmail.com>
2021-08-25 14:15:55 +0100

    Modified the program class to talk to the database by inserting the connectionstring.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WorkerServicesUsingEntityFramework.Services;
 
 namespace WorkerServicesUsingEntityFramework
 {
@@ -18,6 +21,17 @@ namespace WorkerServicesUsingEntityFramework
             Host.CreateDefaultBuilder(args)
                 .ConfigureServices((hostContext, services) =>
                 {
+                    IConfiguration configuration = hostContext.Configuration;
+
+                    //We use the variable to map to the AppDbContext which has necessary information for talking to the database.
+                    var optionBuilder = new DbContextOptionsBuilder<AppDbContext>();
+                    
+                    //The line of code below is what we use to map to Sql by plugging in the name of the connection string in "appsettings.json".
+                    optionBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConn"));
+
+                    //Within the same type of request fronm AppDbContext, we get a new instance of AppDbContext.
+                    services.AddScoped<AppDbContext>(d => new AppDbContext(optionBuilder.Options));
+
                     services.AddHostedService<Worker>();
                 });
     }
