a093af52d364ac1a0693aa6cedcfd3c9240454bf
OLABAYO BALOGUN <olabayobalogun@gmail.com>
2021-08-25 14:26:02 +0100

    Leveraged the ConnectionString property of the "AppSettings" class in order to map to the name of the connection string in the "appsetting.json" file. We can then plug this into the "UseSqlServer" method and complete it.

--- Program.cs
+++ Program.cs
@@ -23,11 +23,17 @@ namespace WorkerServicesUsingEntityFramework
                 {
                     IConfiguration configuration = hostContext.Configuration;
 
+                    //This is where we leverage the ConnectionString property in the AppSettings class
+                    AppSettings.Configuration = configuration;
+
+                    //Here is where we declare the name of the connection string in the "appsetting.json" file in order to plug this into the method that maps the SQL server to the path it needs to access to get the table
+                    AppSettings.ConnectionString = configuration.GetConnectionString("DefaultConn");
+
                     //We use the variable to map to the AppDbContext which has necessary information for talking to the database.
                     var optionBuilder = new DbContextOptionsBuilder<AppDbContext>();
-                    
+
                     //The line of code below is what we use to map to Sql by plugging in the name of the connection string in "appsettings.json".
-                    optionBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConn"));
+                    optionBuilder.UseSqlServer(AppSettings.ConnectionString);
 
                     //Within the same type of request fronm AppDbContext, we get a new instance of AppDbContext.
                     services.AddScoped<AppDbContext>(d => new AppDbContext(optionBuilder.Options));
